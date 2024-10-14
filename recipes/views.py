a129ed6fb84b0f8292341ce164939495d4f0d273
José Roberto Ferreira <joseroberto.ferreira@gmail.com>
2024-10-14 22:49:49 +0100

    Update site

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
+from __localcode.main import generate_recipe
+
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': [generate_recipe() for _ in range(10)]
+    })
+
+def recipe(request, pk):
+    return render(request, 'recipes/pages/recipe-detail.html', context={
+        'recipe': generate_recipe(),
+        'is_detail_page': True,
+    })
