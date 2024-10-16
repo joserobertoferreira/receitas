20ad661b81e9d60020fe604034810dd1c4b85aa1
José Roberto Ferreira <joseroberto.ferreira@gmail.com>
2024-10-16 23:27:03 +0100

    Detail page

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, render
 
-from __localcode.main import generate_recipe
+from recipes.models import Recipe
 
 
 def home(request):
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': [generate_recipe() for _ in range(10)]
+        # 'recipes': [generate_recipe() for _ in range(10)]
+        'recipes': recipes,
     })
 
-def recipe(request, pk):
+def category(request, category_id):
+    # recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+
+    # if not recipes:
+    #     raise Http404('Category not found.')
+
+    recipes = get_list_or_404(
+        Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+    )
+
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes,
+        'title': f'{recipes[0].category.name} - Category |',
+    })
+
+def recipe(request, id):
+    recipe = Recipe.objects.filter(pk=id, is_published=True).order_by('-id').first()
+
     return render(request, 'recipes/pages/recipe-detail.html', context={
-        'recipe': generate_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
     })
